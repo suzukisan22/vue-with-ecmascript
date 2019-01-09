1b84b4da93291e7aa487ac53d529719efd46dd56
suzuki.r <getty2017@gmail.com>
2019-01-10 00:33:30 +0900

    vue-loaderの追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-var publidDir = __dirname + '/public';
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const publidDir = __dirname + '/public'
+
 module.exports = {
   // 元になるファイル
   // こちらをコンパイルしていく
@@ -10,36 +12,26 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  // どのようなライブラリを使ってコンパイルをしていくかの設定
   module: {
     rules: [
       {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options:  {
-          presets: [
-            ['@babel/env',
-              {
-                  "targets": {
-                      "ie": 11
-                  },
-                  "useBuiltIns": "usage"
-              }
-            ]
-          ]
-        }
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       }
-    ]
+    ],
   },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js' // おまじない
-    }
-  },
-  // webpack_devserverの設定
-  devServer: {
-    historyApiFallback: true,
-    // document_rootになる。
-    contentBase: publidDir
-  }
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 };
